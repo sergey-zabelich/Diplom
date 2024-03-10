c9318968ea566b6709d6672f88ec8feaec0390e8
sergey-zabelich <zabelich19@gmail.com>
2024-03-10 10:55:49 +0300

    Регистрация simple tag для Категорий.

--- main/views.py
+++ main/views.py
@@ -4,13 +4,11 @@ from django.shortcuts import render
 from goods.models import Categories
 
 def index(request):
+   
     
-    categories = Categories.objects.all()
-
     context = {
         'title': 'Всё для дома',
         'content': 'Интернет-магазин мебели "Всё для дома"',
-        'categories': categories
     }
 
     return render(request, 'main/index.html', context)
